1570d206963acfa9e0993d337c34e331f2fa0590
rrouz <ryinrouz94@gmail.com>
2025-04-22 22:20:59 -0700

    feat: Create Monthly H5N1 Cases view component with chart

---
+++ src/views/MonthlyH5N1CasesView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col-md-12">
+        <h2>Monthly H5N1 Cases</h2>
+        <MonthlyCasesChart />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import MonthlyCasesChart from "../components/MonthlyCasesChart.vue";
+</script>
+
+<style scoped>
+
+</style>
