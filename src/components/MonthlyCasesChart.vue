b0c793e8ad52d76005483362e096e7299479088b
rrouz <ryinrouz94@gmail.com>
2025-04-22 22:20:35 -0700

    feat: Add MonthlyCasesChart component

---
+++ src/components/MonthlyCasesChart.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-if="isLoading" class="loading">Loading data...</div>
+    <div v-else-if="error" class="error">{{ error }}</div>
+    <MonthlyBarChart v-else :data="monthlyData" />
+  </div>
+</template>
+
+<script setup>
+//import { ref, onMounted } from 'vue';
+import { MonthlyBarChart } from 'outbreakInfo';
+// TODO: Import API service
+
+
+
+</script>
+
+<style scoped>
+.loading, .error {
+  padding: 20px;
+  text-align: center;
+}
+.error {
+  color: red;
+}
+</style>
