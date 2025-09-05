51a96167f8b4580425a8cd11a2e2d21d5131fdb7
gkarthik <xraydiffraction@gmail.com>
2025-09-05 16:51:02 -0700

    Added gtag

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createGtag } from 'vue-gtag'
 
 const app = createApp(App)
+
+if (import.meta.env.MODE === 'production') {
+    app.use(createGtag({ tagId: 'G-R0C31MMH0Z' }))
+}
+
 app.use(router)
 app.mount('#app')
